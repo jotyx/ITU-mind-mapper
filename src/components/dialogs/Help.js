efca69fa308af32cbe7d7799291121659e9df551
Jiří Čechák <cechak@inqool.cz>
2017-10-23 18:52:36 +0200

    help update

--- src/components/dialogs/Help.js
+++ src/components/dialogs/Help.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { compose, withHandlers } from "recompose";
+import { compose, withHandlers, withState } from "recompose";
 import { reduxForm } from "redux-form";
 import { withRouter } from "react-router-dom";
 import { map } from "lodash";
@@ -15,29 +15,37 @@ const helpData = [
   { title: "Nadpis další nápovědy", text: "Text další nápovědy.", image }
 ];
 
-const Help = ({ handleSubmit, data }) => (
+const Help = ({ handleSubmit, data, showLabel, setShowLabel }) => (
   <DialogContainer
     title="Nápověda"
     name="Help"
     handleSubmit={handleSubmit}
     submitLabel="OK"
   >
-    <Carousel interval={null}>
-      {map(helpData, (item, i) => (
-        <Carousel.Item key={i}>
-          <img width="100%" height="auto" src={item.image} />
-          <Carousel.Caption>
-            <h3>{item.title}</h3>
-            <p>{item.text}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div
+      onMouseEnter={() => setShowLabel(true)}
+      onMouseLeave={() => setShowLabel(false)}
+    >
+      <Carousel interval={null} indicators={false} controls={showLabel}>
+        {map(helpData, (item, i) => (
+          <Carousel.Item key={i}>
+            <img width="100%" height="auto" src={item.image} />
+            {showLabel && (
+              <Carousel.Caption>
+                <h3>{item.title}</h3>
+                <p>{item.text}</p>
+              </Carousel.Caption>
+            )}
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   </DialogContainer>
 );
 
 export default compose(
   withRouter,
+  withState("showLabel", "setShowLabel", false),
   withHandlers({
     onSubmit: dialog => async (formData, dispatch, props) => {
       dialog.closeDialog();
