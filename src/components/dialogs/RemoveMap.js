80b532fb1404b582778c2a36c58495cbd716c713
Jiří Čechák <cechak@inqool.cz>
2017-10-27 00:46:59 +0200

    refactor

--- src/components/dialogs/RemoveMap.js
+++ src/components/dialogs/RemoveMap.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { compose, withHandlers } from "recompose";
-import { reduxForm, Field, SubmissionError } from "redux-form";
+import { reduxForm } from "redux-form";
 import { withRouter } from "react-router-dom";
-import { isEmpty } from "lodash";
 
 import DialogContainer from "./DialogContainer";
-import Input from "../form/Input";
-import * as Validation from "../form/Validation";
 
 import { removeMap } from "../../actions/mapsActions";
 
